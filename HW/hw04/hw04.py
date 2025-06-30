8f17e7a7654ae557cb99be73c4cf594c86b4326b
Equiliw <2714996595@qq.com>
2025-06-30 15:41:40 +0800

    Week 6

--- HW/hw04/hw04.py
+++ HW/hw04/hw04.py
@@ -13,6 +13,10 @@ def shuffle(s):
     """
     assert len(s) % 2 == 0, 'len(seq) must be even'
     "*** YOUR CODE HERE ***"
+    s, mid = list(s), len(s) // 2
+    l, r = s[:mid], s[mid:]
+    return [x for pair in zip(l, r) for x in pair]
+
 
 
 def deep_map(f, s):
@@ -38,6 +42,11 @@ def deep_map(f, s):
     True
     """
     "*** YOUR CODE HERE ***"
+    for i, x in enumerate(s):
+        if (type(x) == list):
+            deep_map(f, x)
+        else:
+            s[i] = f(s[i])
 
 
 HW_SOURCE_FILE=__file__
@@ -47,11 +56,13 @@ def planet(mass):
     """Construct a planet of some mass."""
     assert mass > 0
     "*** YOUR CODE HERE ***"
+    return ['planet', mass]
 
 def mass(p):
     """Select the mass of a planet."""
     assert is_planet(p), 'must call mass on a planet'
     "*** YOUR CODE HERE ***"
+    return p[1]
 
 def is_planet(p):
     """Whether p is a planet."""
@@ -104,6 +115,18 @@ def balanced(m):
     True
     """
     "*** YOUR CODE HERE ***"
+    if is_planet(m):
+        return True
+    else:
+        res = balanced(end(left(m))) and balanced(end(right(m)))
+
+        l_mass = total_mass(end(left(m)))
+        l_torque = length(left(m)) * l_mass
+        r_mass = total_mass(end(right(m)))
+        r_torque = length(right(m)) * r_mass
+
+        res = res and l_torque == r_torque
+        return res
 
 
 def berry_finder(t):
@@ -124,6 +147,12 @@ def berry_finder(t):
     True
     """
     "*** YOUR CODE HERE ***"
+    if (label(t) == 'berry'):
+        return True
+    elif (is_leaf(t)):
+        return False
+    else:
+        return any([berry_finder(x) for x in branches(t)])
 
 
 HW_SOURCE_FILE=__file__
@@ -139,6 +168,10 @@ def max_path_sum(t):
     17
     """
     "*** YOUR CODE HERE ***"
+    if (is_leaf(t)):
+        return label(t)
+    else:
+        return label(t) + max([max_path_sum(x) for x in branches(t)])
 
 
 def mobile(left, right):
--- Lab/lab04/lab04.py
+++ Lab/lab04/lab04.py
@@ -7,7 +7,7 @@ def divide(quotients, divisors):
     >>> divide(range(1, 5), range(20, 25))
     {1: [20, 21, 22, 23, 24], 2: [20, 22, 24], 3: [21, 24], 4: [20, 24]}
     """
-    return {____: ____ for ____ in ____}
+    return {q: [d for d in divisors if (d % q == 0)] for q in quotients}
 
 
 def buy(fruits_to_buy, prices, total_amount):
@@ -29,10 +29,10 @@ def buy(fruits_to_buy, prices, total_amount):
             print(cart)
         elif fruits and amount > 0:
             fruit = fruits[0]
-            price = ____
-            for k in ____:
+            price = prices[fruit]
+            for k in range(1, amount // price + 1):
                 # Hint: The display function will help you add fruit to the cart.
-                add(____, ____, ____)
+                add(fruits[1:], amount - price * k, cart + display(fruit, k))
     add(fruits_to_buy, total_amount, '')
 
 
@@ -67,7 +67,8 @@ def distance(city_a, city_b):
     5.0
     """
     "*** YOUR CODE HERE ***"
-
+    f = lambda x, y: sqrt(x ** 2 + y ** 2)
+    return f(get_lat(city_a) - get_lat(city_b), get_lon(city_a) - get_lon(city_b))
 def closer_city(lat, lon, city_a, city_b):
     """
     Returns the name of either city_a or city_b, whichever is closest to
@@ -84,6 +85,8 @@ def closer_city(lat, lon, city_a, city_b):
     'Bucharest'
     """
     "*** YOUR CODE HERE ***"
+    pos = make_city('P', lat, lon)
+    return get_name(city_b) if distance(pos, city_b) <= distance(pos, city_a) else get_name(city_a)
 
 def check_city_abstraction():
     """
